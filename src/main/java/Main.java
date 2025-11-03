dba6a897e2dc9db7ba4b961a7a0b3e42a30ad80c
Masterkaj2137 <148704613+Masterkaj2137@users.noreply.github.com>
2025-11-03 18:19:41 +0000

    P3_3_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,9 @@ public class Main {
     
     System.out.print("Podaj wysokosc boku: ");
     int n = scanner.nextInt();
-
+    
+//Pierwszy trójkąt
+    
     for (int k = 0; k < n; k++){
 
       for(int i = 0; i <= k; i++){
@@ -15,6 +17,8 @@ public class Main {
       System.out.println();
     }
     System.out.println();
+
+//Drugi trójkąt
     
     for (int k = 0; k < n; k++){
 
@@ -30,6 +34,8 @@ public class Main {
   }
     System.out.println();
 
+//Trzeci trójkąt
+    
     for (int k = 0; k < n; k++){
 
       for(int i = 0; i <(n-k-1); i++){
@@ -41,6 +47,26 @@ public class Main {
       System.out.println();
     }
     System.out.println();
+
+    //Czwarty trójkąt
+
+    for (int k = 0; k < n; k++){
+
+      for(int i = 0; i <(n-k-1); i++){
+        System.out.print(" ");
+      }
+      for(int i =0; i<=k; i++){
+        if(i==0 || i==k || k == n-1){
+          System.out.print("*");
+        }
+        else{
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+
+    
   
   }
 }
